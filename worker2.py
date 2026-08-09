c1408b84ecdf258e10d59ba2cb259b963e64ad5d
amir <136586127+amir225588@users.noreply.github.com>
2026-08-09 16:52:52 +0330

    Create worker2.py

---
+++ worker2.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+
+"""
+لیست شعب و نمایندگی های مجتمع فنی تهران
+ساختار:
+استان -> نمایندگی -> لینک صفحه نمایندگی
+"""
+
+ACADEMIES = {
+
+    "🏙 تهران": {
+
+        "🏢 سعادت آباد (مرکزی)": 
+            "https://mftplus.com/academy/سعادت-آباد",
+
+        "🏢 آیت الله کاشانی (غرب)": 
+            "https://mftplus.com/academy/آیت-الله-کاشانی",
+
+        "🏢 اسلامشهر":
+            "https://mftplus.com/academy/اسلامشهر",
+
+        "🏢 اکباتان":
+            "https://mftplus.com/academy/اکباتان",
+
+        "🏢 انقلاب":
+            "https://mftplus.com/academy/انقلاب",
+
+        "🏢 باملند (شمال غرب)":
+            "https://mftplus.com/academy/باملند",
+
+        "🏢 بلوار کاوه (قیطریه)":
+            "https://mftplus.com/academy/قیطریه",
+
+        "🏢 پارک ساعی":
+            "https://mftplus.com/academy/پارک-ساعی",
+
+        "🏢 پردیس":
+            "https://mftplus.com/academy/پردیس",
+
+        "🏢 پرند":
+            "https://mftplus.com/academy/پرند",
+
+        "🏢 پیروزی":
+            "https://mftplus.com/academy/پیروزی",
+
+        "🏢 تهرانپارس":
+            "https://mftplus.com/academy/تهرانپارس",
+
+        "🏢 رودهن":
+            "https://mftplus.com/academy/رودهن",
+
+        "🏢 زعفرانیه":
+            "https://mftplus.com/academy/زعفرانیه",
+
+        "🏢 سیدخندان":
+            "https://mftplus.com/academy/سیدخندان",
+
+        "🏢 شهرقدس":
+            "https://mftplus.com/academy/شهرقدس",
+
+        "🏢 شهریار":
+            "https://mftplus.com/academy/شهریار",
+
+        "🏢 میرداماد":
+            "https://mftplus.com/academy/میرداماد",
+
+        "🏢 نیاوران":
+            "https://mftplus.com/academy/نیاوران",
+
+        "🏢 ورامین":
+            "https://mftplus.com/academy/ورامین",
+    },
+
+
+    "🌄 آذربایجان شرقی": {
+
+        "🏢 تبریز":
+            "https://mftplus.com/academy/تبریز",
+
+        "🏢 مرند":
+            "https://mftplus.com/academy/مرند",
+    },
+
+
+    "🌄 اصفهان": {
+
+        "🏢 شاهین شهر":
+            "https://mftplus.com/academy/شاهین-شهر",
+    },
+
+
+    "🌄 البرز": {
+
+        "🏢 کرج":
+            "https://mftplus.com/academy/کرج",
+
+        "🏢 فردیس":
+            "https://mftplus.com/academy/فردیس",
+    },
+
+
+    "🌊 بوشهر": {
+
+        "🏢 بوشهر":
+            "https://mftplus.com/academy/بوشهر",
+
+        "🏢 کنگان":
+            "https://mftplus.com/academy/کنگان",
+    },
+
+
+    "🌄 خراسان جنوبی": {
+
+        "🏢 بیرجند":
+            "https://mftplus.com/academy/بیرجند",
+    },
+
+
+    "🌄 خراسان رضوی": {
+
+        "🏢 مشهد":
+            "https://mftplus.com/academy/مشهد",
+
+        "🏢 سبزوار":
+            "https://mftplus.com/academy/سبزوار",
+    },
+
+
+    "🌄 زنجان": {
+
+        "🏢 زنجان":
+            "https://mftplus.com/academy/زنجان",
+    },
+
+
+    "🌄 سمنان": {
+
+        "🏢 سمنان":
+            "https://mftplus.com/academy/سمنان",
+    },
+
+
+    "🌄 فارس": {
+
+        "🏢 شیراز":
+            "https://mftplus.com/academy/شیراز",
+    },
+
+
+    "🌄 قم": {
+
+        "🏢 قم":
+            "https://mftplus.com/academy/قم",
+    },
+
+
+    "🌄 کرمانشاه": {
+
+        "🏢 کنگاور":
+            "https://mftplus.com/academy/کنگاور",
+    },
+
+
+    "🌄 گلستان": {
+
+        "🏢 گرگان":
+            "https://mftplus.com/academy/گرگان",
+    },
+
+
+    "🌊 گیلان": {
+
+        "🏢 رشت":
+            "https://mftplus.com/academy/رشت",
+
+        "🏢 لاهیجان":
+            "https://mftplus.com/academy/لاهیجان",
+    },
+
+
+    "🌄 لرستان": {
+
+        "🏢 خرم آباد":
+            "https://mftplus.com/academy/خرم-آباد",
+    },
+
+
+    "🌊 مازندران": {
+
+        "🏢 بابل":
+            "https://mftplus.com/academy/بابل",
+
+        "🏢 جویبار":
+            "https://mftplus.com/academy/جویبار",
+
+        "🏢 چالوس":
+            "https://mftplus.com/academy/چالوس",
+
+        "🏢 ساری":
+            "https://mftplus.com/academy/ساری",
+
+        "🏢 نکا":
+            "https://mftplus.com/academy/نکا",
+
+        "🏢 نور":
+            "https://mftplus.com/academy/نور",
+    },
+
+
+    "🌄 مرکزی": {
+
+        "🏢 اراک":
+            "https://mftplus.com/academy/اراک",
+
+        "🏢 ساوه":
+            "https://mftplus.com/academy/ساوه",
+    },
+
+
+    "🌊 هرمزگان": {
+
+        "🏢 بندرعباس":
+            "https://mftplus.com/academy/بندرعباس",
+
+        "🏢 کیش":
+            "https://mftplus.com/academy/کیش",
+    }
+
+}
