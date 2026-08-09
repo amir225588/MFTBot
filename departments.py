948b28488d0a2f5c5b60515c596d8a3a9923e0ac
amir <136586127+amir225588@users.noreply.github.com>
2026-08-09 16:41:31 +0330

    Create departments.py

---
+++ departments.py
@@ -0,0 +1,80 @@
+# departments.py
+
+DEPARTMENT_BUTTONS = [
+
+    {
+        "title": "💻 فناوری اطلاعات و ارتباطات",
+        "url": "https://mftplus.com/department/فناوری-اطلاعات-و-ارتباطات"
+    },
+
+    {
+        "title": "⚙ علوم مهندسی",
+        "url": "https://mftplus.com/department/علوم-مهندسی"
+    },
+
+    {
+        "title": "⚡ برق و الکترونیک",
+        "url": "https://mftplus.com/department/برق-و-الکترونیک"
+    },
+
+    {
+        "title": "👦 کودک و نوجوان",
+        "url": "https://mftplus.com/department/کودک-و-نوجوان"
+    },
+
+    {
+        "title": "🌍 زبان‌های خارجی",
+        "url": "https://mftplus.com/department/زبانهای-خارجی"
+    },
+
+    {
+        "title": "🩺 دانش سلامت",
+        "url": "https://mftplus.com/department/دانش-سلامت"
+    },
+
+    {
+        "title": "🏛 معماری",
+        "url": "https://mftplus.com/department/معماری"
+    },
+
+    {
+        "title": "👗 فناوری مد و پوشاک",
+        "url": "https://mftplus.com/department/فناوری-مد-و-پوشاک"
+    },
+
+    {
+        "title": "🎨 هنر، سینما و تولید محتوا",
+        "url": "https://mftplus.com/department/هنر،-سینما-و-تولید-محتوا"
+    },
+
+    {
+        "title": "🌐 مرکز همکاری‌های بین‌الملل",
+        "url": "https://mftplus.com/department/مرکز-همکاری-های-بین-الملل"
+    },
+
+    {
+        "title": "✈ تشریفات و گردشگری",
+        "url": "https://mftplus.com/department/تشریفات-و-گردشگری"
+    },
+
+    {
+        "title": "💰 علوم مالی و حسابداری",
+        "url": "https://mftplus.com/department/علوم-مالی-و-حسابداری"
+    },
+
+    {
+        "title": "🚢 صنایع دریایی",
+        "url": "https://mftplus.com/department/صنایع-دریایی"
+    },
+
+    {
+        "title": "📈 مدیریت و کسب‌وکار",
+        "url": "https://mftplus.com/department/مدیریت-و-کسب-و-کار"
+    },
+
+    {
+        "title": "⚖ حقوق",
+        "url": "https://mftplus.com/department/حقوق"
+    }
+
+]
