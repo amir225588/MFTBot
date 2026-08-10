b3c2f9b60980a0dadbb7d125882c6295dd4ff610
amir <136586127+amir225588@users.noreply.github.com>
2026-08-10 12:29:23 +0330

    Delete news.py

--- news.py
+++
@@ -1,7 +0,0 @@
-if text == "📰 اخبار مجتمع فنی تهران":
-
-    return Response.json({
-        "type": "link",
-        "body": "مشاهده آخرین اخبار مجتمع فنی تهران",
-        "url": "https://mftplus.com/news"
-    })
