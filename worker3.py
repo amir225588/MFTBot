ac800f91ed14991352ce94a5c588885bd69b358a
amir <136586127+amir225588@users.noreply.github.com>
2026-08-10 12:27:40 +0330

    Delete worker3.py

--- worker3.py
+++
@@ -1,7 +0,0 @@
-if text == "🏢 مراکز مجتمع فنی تهران":
-
-    return Response.json({
-        "type": "link",
-        "body": "مشاهده مراکز مجتمع فنی تهران",
-        "url": "https://mftplus.com/academies"
-    })
