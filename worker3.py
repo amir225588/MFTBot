0619ea0c697d8f66b3bb4def51d51ebe83f37c53
amir <136586127+amir225588@users.noreply.github.com>
2026-08-10 08:30:03 +0330

    Create worker3.py

---
+++ worker3.py
@@ -0,0 +1,7 @@
+if text == "🏢 مراکز مجتمع فنی تهران":
+
+    return Response.json({
+        "type": "link",
+        "body": "مشاهده مراکز مجتمع فنی تهران",
+        "url": "https://mftplus.com/academies"
+    })
