d276b9283c12a2684b80d93c72337babadd71c31
amir <136586127+amir225588@users.noreply.github.com>
2026-08-10 12:28:37 +0330

    Delete articles.py

--- articles.py
+++
@@ -1,7 +0,0 @@
-if text == "📖 مقالات مجتمع فنی تهران":
-
-    return Response.json({
-        "type": "link",
-        "body": "مطالعه مقالات آموزشی مجتمع فنی تهران",
-        "url": "https://mftplus.com/articles"
-    })
